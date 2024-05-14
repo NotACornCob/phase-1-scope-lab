87df207cd30d7603a58ee9f51e8e90f9fb24e399
Alex <alex.kleinman@gmail.com>
2024-05-14 17:39:54 -0400

    completed lab

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
-// Write your solution in this file!
+// customerName: Declare a variable in global scope called customerName using the var keyword and assign it the value 'bob'.
+
+var customerName = 'bob'; 
+const leastFavoriteCustomer = 'bill';
+
+function upperCaseCustomerName(){
+    return customerName = 'BOB'
+ }
+
+function setBestCustomer(){
+    bestCustomer = 'not bob'
+}
+
+function overwriteBestCustomer(){
+    bestCustomer = 'maybe bob'
+}
+
+function changeLeastFavoriteCustomer(){
+    leastFavoriteCustomer = 'ann'
+}
